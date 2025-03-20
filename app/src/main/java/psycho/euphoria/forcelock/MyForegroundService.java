4a9d23a074974fbdfc06d0723f16b0a6639f324b
grandiloquent <1785907865@qq.com>
2025-03-20 20:32:49 +0800

    更新

--- app/src/main/java/psycho/euphoria/forcelock/MyForegroundService.java
+++ app/src/main/java/psycho/euphoria/forcelock/MyForegroundService.java
@@ -14,10 +14,25 @@ import android.widget.Toast;
 
 public class MyForegroundService extends Service {
 
+    public static final String ACTION_LOCK = "psycho.euphoria.forcelock.LOCK";
     private static final String CHANNEL_ID = "MyForegroundServiceChannel";
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
+        String action = intent != null ? intent.getAction() : null;
+        if (action != null && action.equals(ACTION_LOCK)) {
+            SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+            int time = preferences.getInt("last", 10);
+            TaskSchedulerWM.scheduleTask(this, 0, time);
+            AlarmScheduler.scheduleAlarm(this, 0, time * 2);
+            Toast.makeText(this, time + " 分钟后锁屏", Toast.LENGTH_SHORT).show();
+        }
+        return START_STICKY; // Or other appropriate return value
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
         createNotificationChannel();
         Intent notificationIntent = new Intent(this, MainActivity.class); // Replace MainActivity.class
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, notificationIntent, PendingIntent.FLAG_IMMUTABLE);
@@ -28,12 +43,6 @@ public class MyForegroundService extends Service {
                 .setContentIntent(pendingIntent)
                 .build();
         startForeground(1, notification);
-        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
-        int time = preferences.getInt("last", 10);
-        TaskSchedulerWM.scheduleTask(this, 0, time);
-        AlarmScheduler.scheduleAlarm(this, 0, time * 2);
-        Toast.makeText(this, time + " 分钟后锁屏", Toast.LENGTH_SHORT).show();
-        return START_STICKY; // Or other appropriate return value
     }
 
     @Override
